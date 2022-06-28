fa88a781ab5be16336b3c3522d5eee52407543d0
Alastor2189 <g.lucafarci89@gmail.com>
2022-06-28 20:33:48 +0200

    aggiunta bonus

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,13 +1,23 @@
+@extends('layout.app')
+@section('main')
+    
 <h1>Movies Database</h1>
+<div class="card-movie">
 <ul>
   {{-- {{ dd($movies) }} --}}
+  
   @foreach ($movies as $movie)
       <li>
           <h2>{{ $movie->title }}</h2>
           <h3>Title: {{ $movie->original_title }}</h3>
           <h4>Nationality: {{ $movie->nationality }}</h4>
-          <div>date: {{\Carbon\Carbon::parse($movie->date)->format('d/M/Y')}}</div>
-          <div>vote: {{ $movie->vote }}/10</div>
+          <p>date: {{\Carbon\Carbon::parse($movie->date)->format('d/M/Y')}}</p>
+          <p>vote: {{ $movie->vote }}/10</p>
       </li>
+  
   @endforeach
+  
 </ul>
+</div>
+
+@endsection
